05e5e7f76c66abeb40511290e051ee02db048329
kumor00 <kumor00@hotmail.com>
2024-06-15 14:22:24 +0800

    1

---
+++ .scripts/build_openssl.ps1
@@ -0,0 +1,101 @@
+<#
+    Autobuild script
+#>
+param (
+    # index of OpenSSL to build
+    [int]$index = 0,
+
+    # configure parameters
+    [string]$build_type = 'dll'
+)
+
+function Build-OpenSSL {
+    param ($Branch, $Dst)
+
+    if ($Branch -eq 'openssl-1.1.1') {
+        $fips = ''
+    } else {
+        $fips = 'enable-fips'
+    }
+    if ($build_type -eq 'static') {
+        $options = '-static'
+    } else {
+        $options = ''
+    }
+    if ($Env:VSCMD_ARG_TGT_ARCH -like '*x64') {
+        $target = 'VC-WIN64A'
+    } else {
+        $target = 'VC-WIN32'
+    }
+    perl Configure $fips --prefix=$Dst --openssldir=. $options $target
+    if ($lastexitcode -ne 0) {
+        Write-Host 'Failed to configure'
+        Exit 1
+    }
+
+    nmake
+    if ($lastexitcode -ne 0) {
+        Write-Host 'Failed to make'
+        Exit 1
+    }
+
+    nmake test
+    if ($lastexitcode -ne 0) {
+        Write-Host 'Failed to test'
+        Exit 1
+    }
+
+    nmake install
+    if ($lastexitcode -ne 0) {
+        Write-Host 'Failed to install'
+        Exit 1
+    }
+}
+
+$ErrorActionPreference = 'Stop'
+
+$Env:PATH += ';C:\Strawberry\perl\bin;C:\Program Files\NASM'
+$source = 'https://www.openssl.org/source/'
+$site = Invoke-WebRequest $source
+$links = $site.Links | Where-Object {$_.href -like 'openssl-*.tar.gz'}
+$tarball = $links.href[$index]
+$arch = $Env:VSCMD_ARG_TGT_ARCH
+$basename = $tarball -replace '\.tar\.gz$'
+if ($basename -like 'openssl-1.*') {
+    $branch = $basename -replace '^(openssl-[0-9]+\.[0-9]+\.[0-9]+).*', '$1'
+} else {
+    $branch = $basename -replace '^(openssl-[0-9]+\.[0-9]+).*', '$1'
+}
+
+$zip = "$basename-$arch-$build_type.zip"
+
+$openssl_url = "$source$tarball"
+if (Test-Path -Path $tarball -PathType Leaf) {
+    Write-Host $tarball 'already downloaded'
+} else {
+    Write-Host 'Downloading' $tarball
+    Invoke-RestMethod -Uri $openssl_url -OutFile $tarball
+}
+
+if (-Not (Test-Path -Path $arch -PathType Container)) {
+    $result = New-Item -ItemType Directory -Name $arch
+}
+$build = "$arch/$basename"
+if (Test-Path -Path $build -PathType Container) {
+    Write-Host 'Removing' $build
+    Remove-Item -Recurse -Force $build
+}
+Write-Host 'Extracting' $tarball
+tar -xz -C $arch -f $tarball
+if ($lastexitcode -ne 0) {
+    Write-Host 'Failed to extract the tarball'
+    Exit 1
+}
+
+$dst = '/OpenSSL'
+Set-Location $build
+Build-OpenSSL -Branch $branch -Dst $dst
+Set-Location ../..
+
+Write-Host 'Compressing' $zip
+Compress-Archive -Path $dst -DestinationPath $zip -Force
